6fd951a8d41fe6a24390012d48cf41854cc9b362
znfc <1165103172lin@sina.com>
2017-04-11 16:59:47 +0800

    添加Fragmentt相关代码
    Signed-off-by: znfc <1165103172lin@sina.com>

--- app/src/main/java/linlin/com/fragmentdemo/MainActivity.java
+++ app/src/main/java/linlin/com/fragmentdemo/MainActivity.java
@@ -1,13 +1,24 @@
 package linlin.com.fragmentdemo;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private My_Fragment my_fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (my_fragment == null) {
+            my_fragment = new My_Fragment();
+        }
+        transaction.add(R.id.my_framelayout, my_fragment);
+        transaction.show(my_fragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/linlin/com/fragmentdemo/My_Fragment.java
@@ -0,0 +1,22 @@
+package linlin.com.fragmentdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhaopenglin on 2017/4/11.
+ */
+
+public class My_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.myfragment, container, false);
+        return view;
+    }
+}
